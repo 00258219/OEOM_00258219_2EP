1ffc9bbf8990570ab714a1644a24efd205f8f877
00258219 <00258219@uca.edu.sv>
2020-05-28 02:44:37 -0600

    Add: Images

--- SourceCode/Vista/UserManage.Designer.cs
+++ SourceCode/Vista/UserManage.Designer.cs
@@ -195,7 +195,7 @@ namespace SourceCode
             this.tabPage2.Location = new System.Drawing.Point(4, 31);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(859, 227);
+            this.tabPage2.Size = new System.Drawing.Size(860, 228);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Eliminar Usuario";
             this.tabPage2.UseVisualStyleBackColor = true;
